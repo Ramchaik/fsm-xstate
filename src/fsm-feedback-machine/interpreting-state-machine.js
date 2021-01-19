d3b11c05248fc8bf15f93b8888e6b4bf335dab57
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-19 22:44:25 +0530

    feat(fsm/feedback/interpreter): expose add listener and clean up

--- src/fsm-feedback-machine/interpreting-state-machine.js
+++ src/fsm-feedback-machine/interpreting-state-machine.js
@@ -2,13 +2,37 @@ const { feedbackMachine, transition } = require("./fsm");
 
 function interpret(machine) {
   let currentState = machine.initial;
+  let status = 1; // 1 means started, 2 means stoped
+
+  const listeners = new Set();
+
   const send = (event) => {
+    // Don't accept events if stopped
+    if (status === 2) {
+      console.log("machine stoped");
+      return;
+    }
+
     const nextState = transition(currentState, event);
     console.log(nextState);
     currentState = nextState;
   };
 
-  return { send };
+  const onTransition = (listener) => {
+    listeners.add(listener);
+  };
+
+  // Cleanup
+  const stop = () => {
+    status = 2;
+    listeners.clear();
+  };
+
+  return {
+    onTransition,
+    send,
+    stop,
+  };
 }
 
 module.exports = {
