6096316f1d117cd508d327727b4b90d634cc92d2
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-20 20:36:13 +0530

    feat(fsm): update xstate transition with transition object

--- src/fsm-xstate-feedback-machine/fsm.ts
+++ src/fsm-xstate-feedback-machine/fsm.ts
@@ -3,7 +3,13 @@ import { createMachine } from "xstate";
 export const feedbackMachine = createMachine({
   initial: "question",
   states: {
-    question: {},
+    question: {
+      on: {
+        CLICK_GOOD: {
+          target: "thanks",
+        },
+      },
+    },
     form: {},
     thanks: {},
     closed: {},
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { sendObj, sendSwitch } from "./fsm-promise/interpreter";
 import { send } from "./fsm-feedback-machine/interpreting-state-machine";
 import { feedbackMachine } from "./fsm-xstate-feedback-machine/fsm";
 
+/*
 sendObj("FETCH");
 sendObj("FETCH"); // anomalous state
 sendObj("RESOLVE");
@@ -18,3 +19,18 @@ send("CLICK_BAD");
 send("SUBMIT");
 send("CLOSE");
 console.log(" -------------------------- ");
+*/
+
+console.log(feedbackMachine.initialState);
+const clickGoodEvent = {
+  type: "CLICK_GOOD",
+};
+
+const nextState = feedbackMachine.transition(
+  feedbackMachine.initialState,
+  clickGoodEvent
+);
+
+console.log("**********");
+
+console.log(nextState);
